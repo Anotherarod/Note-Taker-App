b56979f2fd504c0ce50422853048bd15887e7ab5
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-19 22:46:39 -0700

    Added app.get/post functions to link htmls

--- server.js
+++ server.js
@@ -10,3 +10,25 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db.json"));
+});
+
+app.post("/api/notes", function(req, res) {
+ 
+    fs.readFile(path.join(__dirname, "db.json"), "utf8", function(error, response) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(response)
