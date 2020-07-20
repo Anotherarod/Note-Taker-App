a4132396de76737bffcffb54b6ec6ac9cb6bcc65
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-19 21:58:48 -0700

    Added app.use method for express and to take in css styling

--- server.js
+++ server.js
@@ -7,3 +7,6 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
